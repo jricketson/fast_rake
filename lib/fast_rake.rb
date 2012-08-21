6bd0dbbe8e46ba67b477bd7821614757d9e14436
Daniel Heath <daniel.heath@lonelyplanet.com.au>
2012-08-21 13:33:01 +1000

    Remove deprecation warning from newer versions of rake

--- lib/fast_rake.rb
+++ lib/fast_rake.rb
@@ -2,6 +2,7 @@ require 'fast_rake/version'
 require 'fast_rake/fast_runner'
 
 module FastRake
+  extend Rake::DSL
 
   def self.fast_runner(setup_tasks, run_tasks)
     fast_runner_task(:two, 2, setup_tasks, run_tasks)
@@ -10,7 +11,6 @@ module FastRake
   end
 
   def self.fast_runner_task(name, processes, setup_tasks, run_tasks)
-
     desc "Fast test runner for #{processes} cpus"
     task name, [:list] => setup_tasks do |t, args|
       tasks_to_run = if !args[:list].nil?
@@ -21,6 +21,5 @@ module FastRake
       FastRunner.new(tasks_to_run, processes).run
     end
   end
-
 
 end
