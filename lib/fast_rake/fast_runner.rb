621d870caa7cfeab330edd40ba90c94228017fb8
Tim Moore <tim.moore@lonelyplanet.com.au>
2013-03-20 16:00:23 +1100

    Change regex so that missing args are passed as nil instead of an empty string

--- lib/fast_rake/fast_runner.rb
+++ lib/fast_rake/fast_runner.rb
@@ -91,7 +91,7 @@ class FastRake::FastRunner
     STDERR.reopen(output_path.join('stderr'), 'w')
 
     setup_database(task_name)
-    task_match = task_name.match(/([^\[]*)\[?([^\]]*)\]?/)
+    task_match = task_name.match(/([^\[]*)(?:\[([^\]]*)\])?/)
     task_name = task_match[1]
     task_args = task_match[2]
     Rake::Task[task_name].invoke(task_args)
